c69a43976ef2d6875c524163a795dcc7a1b51bb6
Igor Taranenko <igor.t@solity.io>
2021-01-12 17:24:03 +0200

    Completed registation, started login function

--- conrollers/auth.js
+++ conrollers/auth.js
@@ -1,11 +1,58 @@
+const bcrypt = require('bcryptjs');
+const JWT = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 module.exports.login = async (req, res) => {
-    console.log(req.body.email);
-    res.status(200).json({
-        message: {
-            email: req.body.email,
-            password: req.body.password
+    const candidate = await User.findOne({ email: req.body.email });
+    if (candidate) {
+        // Пользователь существует
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = "";
+            res.status(200).json({
+                token: token
+            });
+        } else {
+            res.status(401).json({
+                message: "Неверный пароль! Попробуйте снова"
+            });
         }
+    } else {
+        // Пользователя нет
+        res.status(404).json({
+            message: "Пользователь с таким email не существует."
+        });
+    }
+}
+
+module.exports.register = async (req, res) => {
+    const candidate = await User.findOne({
+        email: req.body.email
     });
+
+    if (candidate) {
+        // Ошибка, пользователь есть
+        res.status(409).json({
+            message: "Пользователь с таким email уже существует!"
+        });
+    } else {
+        // success
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+        try {
+            await user.save();
+            res.status(201).json({
+                message: "Пользователь создан!"
+            });
+        } catch {
+            // Обработка ошибок
+            
+        }
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.post('/login', controller.login);
 
+router.post('/register', controller.register);
+
 module.exports = router;
